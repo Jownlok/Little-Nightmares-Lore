ef2d12a6cb0a415bae47f0344ccd1ef108c0728c
Joownlok <joao.6564693@gmail.com>
2026-09-25 18:43:37 -0300

    Personagens e criadores
    Definido os textos e áudios

--- javascript/pageone.js
+++ javascript/pageone.js
@@ -19,3 +19,35 @@ var botao3 = document.querySelector(".criadores");
     linha.style.width = "0%";
   });
 });
+
+// SFX Botões
+const som = new Audio('../mp3/button_start.ogg');
+
+[botao1, botao2, botao3].forEach(botao => {
+  botao.addEventListener('mouseenter', () => {
+    som.currentTime = 0;
+    som.play();
+  });
+});
+
+// Fade / Musica de inicio em LOOP
+const musicaFundo = new Audio('../mp3/musica.mp3');
+musicaFundo.loop = true;
+
+musicaFundo.play().catch(error => {
+  console.log("O navegador bloqueou o áudio automático.");
+});
+function pararComFadeOut(audio, duracaoEmSegundos = 1.5) {
+  const passos = 20; 
+  const intervaloTempo = (duracaoEmSegundos * 1000) / passos;
+  const decrementoVolume = audio.volume / passos;
+
+  const fade = setInterval(() => {
+    if (audio.volume > decrementoVolume) {
+      audio.volume -= decrementoVolume;
+    } else {
+      audio.volume = 0;audio.pause();clearInterval(fade);
+    }
+  }, intervaloTempo);
+}
+
